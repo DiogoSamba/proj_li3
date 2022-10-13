32e49dfa20ab2bb12dff2b22123636c00d11c90f
Samba <a100714@alunos.uminho.pt>
2022-10-13 19:38:22 +0100

    Resolução de bugs

--- Guioes/1/io.c
+++ Guioes/1/io.c
@@ -33,6 +33,79 @@ void pushCom (char* str, Coms* c)
     c->pos++;                                       //Sets pos to next index of the list
 }
 
+void execCom (char* com, DEQ* d)                    //Função auxiliar ao handle
+{
+    Node* n;
+    Type t;
+    void* data;
+    char* a [10];
+    for (int i= 0; i<10; i++)
+    {
+        a[i]= calloc (5, sizeof (char));
+    }
+    a[0]= "c";
+    a[1]= "pus";
+    a[2]= "pusf";
+    a[3]= "pop";
+    a[4]= "popf";
+    a[5]= "sz";
+    a[6]= "ismt";
+    a[7]= "rev";
+    a[8]= "dest";
+    a[9]= "prnt";
+    if (strcmp ( com, a[0])== 0)
+        d= create();
+    else if (strcmp ( com, a[1])== 0)
+    {
+        printf ("What type of data do you intend to queue? (int, char, ...)\n");
+        //Apanhar o tipo de dados
+        //Interpretar dados segundo o tipo de dados
+        push (d, data, t);
+    }
+    else if (strcmp ( com, a[2])== 0)
+    {
+        printf ("What type of data do you intend to queue? (int, char, ...)\n");
+        //Apanhar o tipo de dados
+        printf ("Enter data:\n");
+        //Interpretar dados segundo o tipo de dados
+        pushFront (d, data, t);
+    }
+    else if (strcmp ( com, a[3])== 0)
+    {
+        n= pop (d);
+        //Apanhar o tipo para escrever
+        printf ("Popped element of type \n");
+    }
+    else if (strcmp ( com, a[4])== 0)
+    {
+        n= popFront (d);
+        //Apanhar o tipo para escrever
+        printf ("Popped element of type \n");
+    }
+    else if (strcmp ( com, a[5])== 0)
+        printf ("%i", size(d));
+    else if (strcmp ( com, a[6])== 0)
+        if (isEmpty)
+            printf ("DEQ is Empty\n");
+        else
+            printf ("DEQ is not Empty\n");
+    else if (strcmp ( com, a[7])== 0)
+    {
+        reverse (d);
+        printf ("DEQ has been reversed.\n");
+    }
+    else if (strcmp ( com, a[8])== 0)
+    {    
+        destroy (d);
+        printf ("DEQ has been destroyed.\n");
+    }
+    else if (strcmp ( com, a[9])== 0)
+    {
+        printf ("Full DEQ:\n");
+        printDEQ (d);
+    }
+}
+
 void handle ()                                      //Function to handle input
 {
     char* in= malloc (BUFSIZ);
@@ -45,61 +118,10 @@ void handle ()                                      //Function to handle input
         s= strtok (NULL, " \n");
     }
     DEQ* d;
-    for (int i= 0; i< c->sz; i++)                           //Interpreta cada comando da lista
+    for (int i= 0; i< c->pos; i++)                          //Goes through every command in the list
     {
-        Type t;
-        void* data;
-        Node* n;
         printf ("Comando #%i, %s:\n", i, c->list[i]);
-        if (strcmp ( c->list[i], "c"))
-            d= create();
-        else if (strcmp ( c->list[i], "pus"))
-        {
-            printf ("What type of data do you intend to queue? (int, char, ...)\n");
-            //Apanhar o tipo de dados
-            //Interpretar dados segundo o tipo de dados
-            push (d, data, t);
-        }
-        else if (strcmp ( c->list[i], "pusf"))
-        {
-            printf ("What type of data do you intend to queue? (int, char, ...)\n");
-            //Apanhar o tipo de dados
-            printf ("Enter data:\n");
-            //Interpretar dados segundo o tipo de dados
-            pushFront (d, data, t);
-        }
-        else if (strcmp ( c->list[i], "pop"))
-        {
-            n= pop (d);
-            //Apanhar o tipo para escrever
-            printf ("Popped element of type \n");
-        }
-        else if (strcmp ( c->list[i], "popf"))
-        {
-            n= popFront (d);
-            //Apanhar o tipo para escrever
-            printf ("Popped element of type \n");
-        }
-        else if (strcmp ( c->list[i], "sz"))
-            printf ("%i", size(d));
-        else if (strcmp ( c->list[i], "ismt"))
-            if (isEmpty)
-                printf ("DEQ is Empty\n");
-            else
-                printf ("DEQ is not Empty\n");
-        else if (strcmp ( c->list[i], "rev"))
-        {
-            reverse (d);
-            printf ("DEQ has been reversed.\n");
-        }
-        else if (strcmp ( c->list[i], "dest"))
-        {    
-            destroy (d);
-            printf ("DEQ has been destroyed.\n");
-        }
-        else if (strcmp ( c->list[i], "prnt"))
-            printf ("Full DEQ:\n");
-            printDEQ (d);
+        execCom (c->list[i], d);                            //Executes the command at the i index of the array
     }
 }
 
