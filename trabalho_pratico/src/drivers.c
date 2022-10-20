5abc7ad96ae2a94049f741b8ef55a3135de9b280
Samba.lnv <diogofontes2003@gmail.com>
2022-10-20 13:26:41 +0100

    Início do trabalho de LI3

---
+++ trabalho_pratico/src/drivers.c
@@ -0,0 +1,12 @@
+//id;name;birth_day;gender;car_class;license_plate;city;account_creation;account_status
+struct driver
+{
+    int id;
+    char* name;
+    int data_nasc;
+    char class;
+    char license_plate[7];
+    char cidade;
+    int acc_c;
+    char active;
+}
