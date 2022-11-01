ce3b65f130cd9ad4c04bcd953722711526d7876a
Samba <a100714@alunos.uminho.pt>
2022-11-01 22:32:44 +0000

    Planeamento rides.c

--- trabalho_pratico/src/rides.c
+++ trabalho_pratico/src/rides.c
@@ -7,3 +7,25 @@
         data ocurrencia
 */
 
+/* Parametros: 
+    Cidade
+    Preço
+    Data
+    City is a Driver caracteristic.
+    Avaliação -> Condutor
+    Flag Gorjeta
+*/   
+#include "rides.h"
+
+typedef struct ride_a
+{
+    int date[3];
+    int driver;
+    int user;
+    char city;
+    int distance;
+    int score_u;
+    int score_d;
+    int tip;
+    char* com;
+}ride_a;
