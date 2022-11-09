622f5b1a2e34442c9011bcad0c8265f9b74f8733
Luís Ribeiro <a100608@alunos.uminho.pt>
2022-11-09 12:58:34 +0000

    mini alterações

--- trabalho_pratico/src/io.c
+++ trabalho_pratico/src/io.c
@@ -5,17 +5,11 @@ void q1 ()
     char id[30];
     scanf("%29s", id);
     int n;
-    /*
-    Driver res;
     if (id[0] >= '0' && id[0] <= '9')
     {
         n = atoi(id);
-        res = get_driver_id(a, n);
-        print_driver(res);
-    } */   
-    
-    
-    printf("%s\n", id);
+        
+    }   
 }
 
 
@@ -28,11 +22,11 @@ void In ()
     char c = getchar();
     switch (c)
     {
-        /*
         case '1' : q1(); return;
+        /*
         case '2' : q2(); return;
         case '3' : q3(); return;
-         case '4' : q4(); return;
+        case '4' : q4(); return;
         case '5' : q5(); return;
         case '6' : q6(); return;
         case '7' : q7(); return;
@@ -41,9 +35,3 @@ void In ()
         default: return;
     }
 }
-
-/* int main ()
-{
-    In();
-    return 0;
-} */
--- trabalho_pratico/src/main.c
+++ trabalho_pratico/src/main.c
@@ -15,10 +15,9 @@ void set_database_driver (Array_Driver a_d)
     getline (&lino, &len, f_drivers);
     free (lino);
     lino = NULL;
-    while ((c = fgetc(f_drivers)) != EOF)
+    while (getline (&lino, &len, f_drivers) != -1)
     {
         d.name = calloc (1,30);
-        getline (&lino, &len, f_drivers);
         sscanf (lino, "%d;%[^;];%d/%d/%d;%c;%c%[^;];%2s-%2s-%2s;%c%[^;];%d/%d/%d;%c", &d.id, d.name, &d.birth_date[0], &d.birth_date[1], &d.birth_date[2],
         &d.gender, &d.class, lino, &d.license_plate[0], &d.license_plate[2], &d.license_plate[4], &d.city, lino, 
         &d.acc_creat[0], &d.acc_creat[1], &d.acc_creat[2], &d.acc_stat);
@@ -54,19 +53,17 @@ void set_database_user (Array_User a_u)
 {
     FILE * f_drivers = fopen ("../Dataset/users.csv", "r");
     char * lino = NULL;
-    int id = 1, c;
     Name_Tree name_t = init_name_tree ();
     User d;
     size_t len;
     getline (&lino, &len, f_drivers);
     free (lino);
     lino = NULL;
-    while ((c = fgetc(f_drivers)) != EOF)
+    while (getline (&lino, &len, f_drivers) != -1)
     {
         d.name = calloc (1,30);
         d.user_n = calloc (1,30);
         char determinar [3];
-        getline (&lino, &len, f_drivers);
         sscanf (lino, "%[^;];%[^;];%c;%d/%d/%d;%d/%d/%d;%2s%[^;];%c", d.user_n, d.name, &d.gender, &d.birth_date[0], &d.birth_date[1], &d.birth_date[2],
         &d.acc_creat[0], &d.acc_creat[1], &d.acc_creat[2], determinar, lino, &d.acc_status);
         determinar_pay_meth (determinar[1], &d);
