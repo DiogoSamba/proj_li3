4c182ef4f3efd932da132f094a9eb02a19434a3b
Samba.lnv <a100714@alunos.uminho.pt>
2022-11-09 16:41:35 +0000

    Remake Users

--- trabalho_pratico/src/users.c
+++ trabalho_pratico/src/users.c
@@ -23,12 +23,12 @@ typedef struct node
     int size;
 }node;
 
-typedef struct name_tree
+typedef struct node_tree
 {
     node* array_node;
     int pos;
     int size;
-}name_tree;
+}node_tree;
 
 typedef struct array_user
 {
@@ -56,87 +56,24 @@ static void push_array_user (array_user* a, User u)                             
     a->user_array[a->pos++]= u;
 }
 
-//--------Name_Tree
+//--------Tree
 
-static void more_node_nt (name_tree* t)
+static node_array* init_array_node ()
+
+static node_tree* init_node_tree ()
 {
-    t->size<<= 1;
-    t->array_node= realloc (t->array_node, t->size);
-    memset ((void*)&t->array_node[t->pos], 0, (t->size>>1)* sizeof (node));
+    node_tree* t= malloc (sizeof (node_tree));
+    
 }
-
-static void new_node_nt (name_tree* t, char* name, int id)
-{
-    if (t->pos== t->size)
-        more_node_nt (t);
-    node* n= &t->array_node[t->pos++];
-    n->name= name;
-    n->id= id;
-}
-
-static void init_array_node (node* n)
-{
-    n->array_node= calloc (SIZEARRAYNODE, sizeof (node));
-    n->pos= 0;
-    n->size= SIZEARRAYNODE;
-}
-
-static void more_node_node_array (node* n)                                      //Doubles the size of the node array refered by a node
-{
-    n->size<<= 1;
-    n->array_node= realloc (n->array_node, n->size);
-    memset (&n->array_node[n->pos], 0, (n->size>>1)* sizeof(node));
-}
-
-static void new_node_node_array (node* n, char* name, int id)                   //Adds a new node to the node array
-{
-    if (n->pos== n->size)
-        more_node_node_array (n);
-    n= &n->array_node[n->pos++];
-    n->id= id; 
-    n->name= name;
-}
-
-static void push_name_node (node* n, int l, char* name, int id)                 //Builds node tree with minimum depth
-{
-    if (n->name==NULL)
-    {
-        n->name= name;                                                              
-        n->id= id;
-        return;
-    }
-    if (n->array_node==NULL)
-    {
-        init_array_node(n);
-        push_name_node (n->array_node, l+1, n->name, n->id);                        //Pushes the old termination node into the following node array
-        push_name_node (n, l, name, id);                                            //Pushes the argument name/id pair into the reworked tree
-        return;
-    }
-    int i= 0, bin= 0;
-    for (; i< n->size; i++) if (n->array_node[i].name[l]== name[l]) bin= 1;
-    if (!bin)                                                                       //Doesn't find node with the corresponding "l"th letter.
-    {
-        new_node_node_array (n, name, id);
-    }
-    push_name_node (&n->array_node[i], l+1, name, id);                              //If node with corresponding letter is found, fucntion is called recursively to the higher level corresponding node
-}
-
-static void push_name_tree (name_tree* t, char* name, int id)                   //Pushes a name into the name tree Data Structure
-{
-    int bin= 0, i= 0;
-    node* n;
-    for (; i< t->size && !bin; i++) if (t->array_node[i].name[0]== name[0]) bin= 1; //Finds the corresponing node of level 0 in the Name_Tree
-    if (!bin)                                                                       //If nothing was found gets a new node with name's first letter
-    {
-        new_node_nt (t, name, id);
-    }
-    n= &t->array_node[i];                                                           //Node with the corresponding first letter 
-    push_name_node (n, 0, name, id);
-}
-
 
 //--------API--------
 
+
+node_tree* init_node_tree ()
+{
+    node* n= malloc (sizeof (node));
+    n->array_node
+}
 
 array_user* init_array_user ()                                                  //Returns a User Array
 {
@@ -147,17 +84,8 @@ array_user* init_array_user ()                                                  
     return a;
 }
 
-name_tree* init_name_tree ()                                                    //Returns a Name Tree
-{
-    name_tree* n= malloc (sizeof (name_tree));
-    n->array_node= malloc (SIZEARRAYNODE* sizeof (node));
-    n->pos= 0;
-    n->size= SIZEARRAYNODE;
-    return n;
-}
-
-void push_user (User u, array_user* a, name_tree* t)                            //Pushes a User into the data structure
+void push_user (User u, array_user* a, node* n)                            //Pushes a User into the data structure
 {
     push_array_user (a, u);
-    push_name_tree (t, u.name, a->pos);
+    push_tree (u.name, a->pos);
 }
--- trabalho_pratico/src/users.h
+++ trabalho_pratico/src/users.h
@@ -12,8 +12,10 @@ typedef struct user
     char pay_meth;                                                                  //0 Cash 1 CCard 2 DCard
     char acc_status;
 }User;
+
+
 typedef struct array_user* Array_User;
-typedef struct name_tree* Name_Tree;
+typedef struct node_tree* Node_Tree;
 
 Name_Tree init_name_tree ();                                                    //Returns a Name Tree
 Array_User init_array_user ();                                                  //Returns a User Array
